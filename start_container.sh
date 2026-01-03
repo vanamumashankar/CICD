e10bb059026fb4270e38f6a55d29a2201844f1c3
Uma Shankar <105194801+vanamumashankar@users.noreply.github.com>
2026-01-03 12:50:50 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,10 +2,11 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker push umashankar2409/simple-python-flask-app:latest
+docker pull umashankar2409/simple-python-flask-app:latest
 
 # Run the Docker image as a container
 docker run -d -p 6000:6000  umashankar2409/simple-python-flask-app:latest
+
 
 
 
