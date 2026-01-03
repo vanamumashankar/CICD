a18b530278ebda7c3180c54efb299ce08ce3fcab
Uma Shankar <105194801+vanamumashankar@users.noreply.github.com>
2026-01-03 12:50:25 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -5,7 +5,8 @@ set -e
 docker push umashankar2409/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-docker run -d -p 6000:5000  umashankar2409/simple-python-flask-app:latest
+docker run -d -p 6000:6000  umashankar2409/simple-python-flask-app:latest
+
 
 
 
